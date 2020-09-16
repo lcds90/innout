18f4b24d57caeb8afcad8f043bd74eff9c4d7bbc
Leonardo Santos <44562511+lcds90@users.noreply.github.com>
2020-09-15 23:11:54 -0300

    layout login

--- public/index.php
+++ public/index.php
@@ -1,19 +1,5 @@
 <?php 
 
 require_once(dirname(__FILE__, 2) . '/src/config/config.php');
-require_once(dirname(__FILE__, 2) . '/src/models/User.php');
+require_once(dirname(__FILE__, 2) . '/src/views/login.php');
 
-//echo User::getSelect(['id' => 1], 'name, email');
-//echo User::getSelect(['name' => 'Chaves', 'email' => 'chaves@cod3r.com.br']);
-
-print_r(User::get(['id' => 2], 'name, email'));
-echo '<hr>';
-print_r(User::get());
-echo '<hr>';
-print_r(User::get([], 'name'));
-echo '<hr>';
-
-foreach(User::get([], 'name') as $user){
-    echo $user->name;
-    echo '<br>';
-}
---
+++ src/views/login.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/comum.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/icofont.min.css">
+    <link rel="stylesheet" href="assets/css/login.css">
+    <title>In N' Out</title>
+</head>
+
+<body>
+    <form class="form-login" action="#" method="post">
+        <div class="login-card card">
+            <div class="card-header bg-dark">
+                <span class="font-italic welcome">Bem vindo!</span>
+                <i class="icofont-travelling mr-2 text-white"></i>
+                <span class="font-weight-light text-white">In</span>
+                <span class="font-weight-bold mx-1 text-secondary"> N' </span>
+                <span class="font-weight-light text-white">Out</span>
+                <i class="icofont-runner-alt-1 ml-2 text-white"></i>
+            </div>
+            <div class="card-body">
+                <div class="form-group">
+                    <label for="email">E-mail</label>
+                    <input type="email" name="email" id="email" class="form-control" placeholder="Informe o e-mail" autofocus="true" autocomplete="off">
+                </div>
+                <div class="form-group">
+                    <label for="password">Senha</label>
+                    <input type="email" name="password" id="password" class="form-control" placeholder="Informe a senha" autocomplete="off">
+                </div>
+            </div>
+            <div class="card-footer">
+                <button class="btn btn-outline-primary btn-block">Entrar</button>
+            </div>
+        </div>
+    </form>
+</body>
+
+</html>
