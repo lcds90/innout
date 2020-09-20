f760f2026ec3e6db01a6b00bf08f9658a88d8cc7
Leonardo Santos <44562511+lcds90@users.noreply.github.com>
2020-09-20 19:42:43 -0300

    footer

--- src/views/template/footer.php
+++ src/views/template/footer.php
@@ -1,5 +1,6 @@
 <footer class="footer">
-    <span>Desenvolvido com PHP</span>
+    <span>Desenvolvido com </span> 
+    <span class="text-primary ml-1"> PHP</span>
     <span><i class="icofont-heart text-danger mx-1"></i></span>
 </footer>
 <script src="assets/js/app.js"></script>
