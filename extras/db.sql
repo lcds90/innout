2598d0c77dd601639ae0784f77f459d0267c89ee
Leonardo Santos <lcds90@gmail.com>
2021-03-03 15:33:07 -0300

    Update db.sql

--- extras/db.sql
+++ extras/db.sql
@@ -1,5 +1,3 @@
-USE INNOUT;
-
 DROP TABLE IF EXISTS working_hours, users;
 CREATE TABLE users (
     id INT(6) AUTO_INCREMENT PRIMARY KEY, 
@@ -24,18 +22,9 @@ CREATE TABLE working_hours (
     CONSTRAINT cons_user_day UNIQUE (user_id, work_date)
 );
 
--- Essa senha criptografada corresponde ao valor "a"
+-- senha criptografada corresponde ao valor "a"
 INSERT INTO users (id, name, password, email, start_date, end_date, is_admin)
-VALUES (1, 'Admin', '$2y$10$/vC1UKrEJQUZLN2iM3U9re/4DQP74sXCOVXlYXe/j9zuv1/MHD4o.', 'admin@cod3r.com.br', '2000-1-1', null, 1);
+VALUES (1, 'Admin', '$2y$10$/vC1UKrEJQUZLN2iM3U9re/4DQP74sXCOVXlYXe/j9zuv1/MHD4o.', 'admin@lcds.com.br', '2000-1-1', null, 1);
 
 INSERT INTO users (id, name, password, email, start_date, end_date, is_admin)
-VALUES (2, 'Chaves', '$2y$10$/vC1UKrEJQUZLN2iM3U9re/4DQP74sXCOVXlYXe/j9zuv1/MHD4o.', 'chaves@cod3r.com.br', '2000-1-1', null, 1);
-
-INSERT INTO users (id, name, password, email, start_date, end_date, is_admin)
-VALUES (3, 'Seu Barriga', '$2y$10$/vC1UKrEJQUZLN2iM3U9re/4DQP74sXCOVXlYXe/j9zuv1/MHD4o.', 'barriga@cod3r.com.br', '2000-1-1', null, 0);
-
-INSERT INTO users (id, name, password, email, start_date, end_date, is_admin)
-VALUES (4, 'Seu Madruga', '$2y$10$/vC1UKrEJQUZLN2iM3U9re/4DQP74sXCOVXlYXe/j9zuv1/MHD4o.', 'madruga@cod3r.com.br', '2000-1-1', null, 0);
-
-INSERT INTO users (id, name, password, email, start_date, end_date, is_admin)
-VALUES (5, 'Quico', '$2y$10$/vC1UKrEJQUZLN2iM3U9re/4DQP74sXCOVXlYXe/j9zuv1/MHD4o.', 'quico@cod3r.com.br', '2000-1-1', '2019-1-1', 0);
+VALUES (2, 'Teste', '$2y$10$/vC1UKrEJQUZLN2iM3U9re/4DQP74sXCOVXlYXe/j9zuv1/MHD4o.', 'teste@lcds.com.br', '2000-1-1', null, 1);
